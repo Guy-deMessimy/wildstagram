323203274f4b738a937fadf882f89eebe668d6a2
Guy de Messimy <gdemessimy@gmail.com>
2022-02-16 17:07:44 +0100

    add swip camera

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, useRef } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 import { Camera } from "expo-camera";
 import * as ImageManipulator from "expo-image-manipulator";
 
 export default function CameraScreen() {
   const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
   const cameraRef = useRef(null);
 
   useEffect(() => {
@@ -21,27 +22,60 @@ export default function CameraScreen() {
     return <Text>No access to camera</Text>;
   }
   return (
-    <>
-      <Camera style={styles.camera} ref={cameraRef} />
-      <Button
-        title="Take a picture"
-        color="#f194ff"
-        onPress={async () => {
-          const pictureMetadata = await cameraRef.current.takePictureAsync();
-          console.log("pictureMetadata", pictureMetadata);
-          console.log(
-            await ImageManipulator.manipulateAsync(pictureMetadata.uri, [
-              { resize: { width: 800 } },
-            ])
-          );
-        }}
-      />
-    </>
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type} ref={cameraRef}>
+        <Button
+          title="Take a picture"
+          color="#f194ff"
+          onPress={async () => {
+            const pictureMetadata = await cameraRef.current.takePictureAsync();
+            console.log("pictureMetadata", pictureMetadata);
+            console.log(
+              await ImageManipulator.manipulateAsync(pictureMetadata.uri, [
+                { resize: { width: 800 } },
+              ])
+            );
+          }}
+        />
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   camera: {
     flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
   },
 });
